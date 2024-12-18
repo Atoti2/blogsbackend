791c184eb7de5eb9fbe689a6aee1457a2944511e
Your Name <you@example.com>
2024-12-18 14:33:11 +0100

    backend

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 import express from 'express'
 import cors from 'cors'
+import { removeFromCloud } from './cloudinaryConfig.js'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.delete('/post/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        removeFromCloud(id)
+        res.json({msg: "Sikeres törlés!"})
+    } catch (error) {
+        console.log(error);
+        
+    }
+})
+
+app.listen(PORT, () => console.log("Server listening on port: " + PORT))
